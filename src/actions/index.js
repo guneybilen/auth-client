eaaec39ba34f00f306f1aff1e3d4a3f78f7dc3bf
guney bilen <guneybilen@yahoo.com>
2018-04-11 00:47:10 +0300

    video lectures end

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,25 @@
 
-import axios from 'axios'
+import axios from 'axios';
+
+import $ from 'jquery';
 
 // browserHistory communicates information about the 
 // URL to react-router. browserHistory can also be used
 // for making changes for the URL as well.
 import { browserHistory } from 'react-router';
 
-import { AUTH_USER, AUTH_ERROR, UNAUTH_USER } from './types';
+import { AUTH_USER, AUTH_ERROR, UNAUTH_USER, FETCH_MESSAGE } from './types';
 
-const ROOT_URL = 'http://localhost:3090'
+const ROOT_URL = 'http://localhost:3090';
+
+const SIGNUP_URL = 'http://localhost:3090/signup';
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  };
+}
 
 
 // Redux thunk middleware gives you direct access to the dispatch method.
@@ -56,12 +67,48 @@ export function signinUser({ email, password }) {
   }
 }
 
-export function authError(error) {
-  return {
-    type: AUTH_ERROR,
-    payload: error
-  };
+export function signupUser({ email, password }) {
+
+  return function (dispatch) {
+    var ajaxRequest = $.ajax({
+      url: SIGNUP_URL,
+      method: 'POST',
+      dataType: 'json',
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify({ email: email, password: password })
+    })
+
+    ajaxRequest.done(function (data) {
+      // console.log(data);
+      dispatch({ type: AUTH_USER });
+      // console.log(data);
+      localStorage.setItem('token', data.token);
+      browserHistory.push('/feature');
+    })
+
+    ajaxRequest.fail(function (jqXHR, textStatus) {
+      //console.log(jqXHR.responseJSON.error);
+      //console.log(textStatus); // gives: error
+      dispatch(authError(jqXHR.responseJSON.error))
+    })
+
+    // axios post does not return server response. They say online
+    // it is regarding a problem about CORS. But, on the contrary
+    // the problem is solved by using jQuery for 'POST' requests
+    // instead of using axios.
+    /*     axios.post(`${ROOT_URL}/signup`, { email, password })
+          .then(response => {
+            dispatch({ type: AUTH_USER });
+            localStorage.setItem('token', response.data.token);
+            browserHistory.push('/feature');
+          })
+          .catch(response => {
+            console.log(response);
+            dispatch(authError(response.error))
+          }); */
+  }
 }
+
 
 export function signoutUser(error) {
   localStorage.removeItem('token');
@@ -69,4 +116,19 @@ export function signoutUser(error) {
   return {
     type: UNAUTH_USER
   };
+}
+
+export function fetchMessage() {
+  return function (dispatch) {
+    axios.get(ROOT_URL, {
+      headers: { authorization: localStorage.getItem('token') }
+    })
+      .then(response => {
+        // console.log(response)
+        dispatch({
+          type: FETCH_MESSAGE,
+          payload: response.data.message
+        });
+      });
+  }
 }
--- src/index.js
+++ src/index.js
@@ -12,19 +12,36 @@ import App from './components/app';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
-
+import Feature from './components/feature';
+import RequireAuth from './components/auth/require_auth';
+import Welcome from './components/welcome';
 import reducers from './reducers';
+import { AUTH_USER } from "./actions/types";
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
+// create the store earlier than the token check,
+// in order to be able to make a token check.
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem('token');
+// If we have token, consider the user to be signed in.
+if (token) {
+  // we need to update the application state.
+  // the dispatch we have been using in the 
+  // action creators is a property of the store.
+  store.dispatch({ type: AUTH_USER });
+}
+
 ReactDOM.render(
   // Provider is what communicates with our connected components.
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Welcome} />
         <Route path="signin" component={Signin} />
         <Route path="signout" component={Signout} />
         <Route path="signup" component={Signup} />
+        <Route path="feature" component={RequireAuth(Feature)} />
       </Route>
     </Router>
   </Provider>
